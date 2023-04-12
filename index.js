b51084ebfd2f41be29810b77f610f88f8c4abd74
ivanchino <acevedoamaroivan@gmail.com>
2023-04-11 23:51:27 -0600

    Curso pausado

--- index.js
+++ index.js
@@ -1,36 +1,93 @@
+/*Definición de clases*/
 class Cliente 
 {
     nombreCliente;
     ineCliente;
-    numeroCuenta;
-    saldoCuenta;
     rfcCliente;
 }
 
+class cuentaCorriente
+{
+    numero;
+    #saldo;
+    agencia;
+
+    constructor() {
+        this.#saldosaldo = 0;
+        this.numero = '';
+        this.agencia = '';
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+    }
+
+    retiroDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+    }
+
+}
+
+cuentaDeLeonardo = new cuentaCorriente();
+
+cuentaDeLeonardo.depositoEnCuenta(100);
+console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.retiroDeCuenta(100);
+console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(-10);
+console.log(cuentaDeLeonardo);
+
+
+
+
+
+
+
+/* Codigo clases anteriores
 const cliente1 = new Cliente();
 
 cliente1.nombreCliente = 'Jose';
 cliente1.ineCliente = '12345';
-cliente1.numeroCuenta = '12345678'
-cliente1.saldoCuenta = 2000;
+cliente1.rfcCliente = 'SDFAFD231234FQDcle';
+
+const cuentaCorriente1 =  new cuentaCorriente();
+cuentaCorriente1.numero = '12345678'
+cuentaCorriente1.saldo = 2000;
+cuentaCorriente1.agencia = 1001;
 
 const cliente2 = new Cliente();
 
 cliente2.nombreCliente = "Leonardo";
 cliente2.ineCliente = "12345426234";
-cliente2.numeroCuenta = "12345678";
-cliente2.saldoCuenta = 1000;
+cliente2.rfcCliente = '324132440QWNJASL';
+
+const cuentaCorriente2 =  new cuentaCorriente();
+cuentaCorriente2.numero = "12345678";
+cuentaCorriente2.saldo = 1000;
+cuentaCorriente2.agencia = 1002;
 
 const cliente3 = new Cliente();
 
 cliente3.nombreCliente = "Ivan";
 cliente3.ineCliente = "987654";
-cliente3.numeroCuenta = "4665";
-cliente3.saldoCuenta = 1000;
+cliente3.rfcCliente = ' AEAI920216HQTCV05';
 
-console.log(cliente1);
-console.log(cliente2);
-console.log(cliente3);
+const cuentaCorriente3 =  new cuentaCorriente();
+cuentaCorriente3.numero = "4665";
+cuentaCorriente3.saldo = 1000;
+cuentaCorriente3.agencia = 1003;
+
+
+console.log(cuentaCorriente2);
+cuentaCorriente2.depositoEnCuenta(100);
+console.log(cuentaCorriente2);
+cuentaCorriente2.depositoEnCuenta(200);
+console.log(cuentaCorriente2);
+cuentaCorriente2.depositoEnCuenta(500);
+console.log(cuentaCorriente2);
+*/
 
 
 
